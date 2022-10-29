1a93bd7b945f9fd76ffdbad531d999ad1d4e9168
prathamesh29 <prathamesh@byasatech.com>
2022-10-29 22:41:41 +0530

    [New Project]: Created the login page with html css js with the help of firebase

---
+++ login_register.js
@@ -0,0 +1,148 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCvth-sYhJkoh64LvS-CfnwR8I23Askft4",
+  authDomain: "web-auth-76486.firebaseapp.com",
+  projectId: "web-auth-76486",
+  storageBucket: "eb-auth-76486.appspot.com",
+  messagingSenderId: "21378465128",
+  appId: "1:21378465128:web:d620943ad3821c42ba6add",
+  measurementId: "G-4Y50CQXJ4V",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  full_name = document.getElementById("full_name").value;
+  //   favourite_song = document.getElementById("favourite_song").value;
+  //   milk_before_cereal = document.getElementById("milk_before_cereal").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+    // Don't continue running the code
+  }
+  if (
+    validate_field(full_name) == false ||
+    validate_field(email) == false ||
+    validate_field(password) == false
+  ) {
+    alert("One or More Extra Fields is Outta Line!!");
+    return;
+  }
+
+  // Move on with Auth
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        email: email,
+        full_name: full_name,
+        // favourite_song: favourite_song,
+        // milk_before_cereal: milk_before_cereal,
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).set(user_data);
+
+      // DOne
+      alert("User Created!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+    // Don't continue running the code
+  }
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).update(user_data);
+
+      // DOne
+      alert("User Logged In!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false;
+  }
+
+  if (field.length <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
